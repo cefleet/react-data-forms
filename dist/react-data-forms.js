11e2e0953bc6a84c7f833342fbea52a3fafcbac5
Clint Fleetwood <clint.fleetwood@gmail.com>
2020-12-28 14:03:31 -0500

    updated the form to allow for validation

--- dist/react-data-forms.js
+++ dist/react-data-forms.js
@@ -9,22 +9,46 @@ import DateType from "./Elements/DateType";
 const DataForm = ({
   fieldsData,
   onChange,
-  form
+  form,
+  onSubmit = null,
+  submitText = ""
 }) => {
+  //This makes a single array of all of the items in the form.
+  const justFields = fieldsData.flatMap(fs => fs.fields.map(f => f));
+  const [validationErrors, setValidationErrors] = useState([]);
   const [onFieldset, setOnFieldset] = useState(0);
   const elementTypes = {
     Input: Input,
     RadioGroup: RadioGroup,
     Dropdown: Dropdown,
     DateType: DateType
-  };
+  }; //handle validation here
+
+  const formSubmitted = async () => {
+    //validate first!
+    //these are the ones that need to be validated
+    let validateList = justFields.filter(f => f.validate); //for now just dealing with notNull
+
+    let errors = validateList.map(f => f.validate === 'not-null' && !form[f.name] ? {
+      name: f.name,
+      error: 'Empty Value Not Allowed'
+    } : null).filter(f => f); //TODO Additional validators.
+    //IF there are errors this will not allow it to be sent
+
+    if (errors.length > 0) return setValidationErrors(errors); //do the submission. If an error is returned add the error here.
+
+    errors = await onSubmit(form);
+    if (!errors) return;
+    if (errors.length > 0) return setValidationErrors(errors);
+  }; //TODO 
+
 
   const createFieldSet = (data, idx, length) => /*#__PURE__*/React.createElement("fieldset", {
     key: idx,
     className: idx === onFieldset ? 'open' : 'closed'
-  }, /*#__PURE__*/React.createElement("legend", null, data.title, onFieldset !== idx && /*#__PURE__*/React.createElement("button", {
+  }, /*#__PURE__*/React.createElement("legend", null, onFieldset !== idx && /*#__PURE__*/React.createElement("button", {
     onClick: () => setOnFieldset(idx)
-  }, "Show Section")), /*#__PURE__*/React.createElement("details", {
+  }, data.title), onFieldset === idx && data.title), /*#__PURE__*/React.createElement("details", {
     open: idx === onFieldset ? true : false
   }, /*#__PURE__*/React.createElement("summary", {
     style: {
@@ -34,23 +58,38 @@ const DataForm = ({
     className: "form-element-group"
   }, data.fields.map(d => {
     const E = elementTypes[d.type] || elementTypes.Input;
-    return /*#__PURE__*/React.createElement(E, _extends({
+    const error = validationErrors.find(e => e.name === d.name);
+    return /*#__PURE__*/React.createElement("div", {
       key: d.name
-    }, d, {
+    }, /*#__PURE__*/React.createElement(E, _extends({}, d, {
       onChange: onChange,
       value: form[d.name]
-    }));
+    })), error && /*#__PURE__*/React.createElement("div", {
+      className: "data-form-error"
+    }, " ^ ", d.title, " : ", error.error));
   })), /*#__PURE__*/React.createElement("div", {
     className: "form-navigation"
   }, idx !== 0 && /*#__PURE__*/React.createElement("button", {
     onClick: () => setOnFieldset(idx - 1)
   }, "Prev"), idx !== length - 1 && /*#__PURE__*/React.createElement("button", {
     onClick: () => setOnFieldset(idx + 1)
-  }, "Next"))));
+  }, "Next")))); //if the error name is found, the error message will show up near that form item.
+  //otherwise it will be a general for error.
+
+
+  const showErrors = () => /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", {
+    className: "data-form-error"
+  }, "There are Errors."), validationErrors.filter(e => !justFields.find(f => e.name === f.name)).map((e, idx) => /*#__PURE__*/React.createElement("div", {
+    key: idx,
+    className: "data-form-error"
+  }, e.name, ": ", e.error)));
 
   return /*#__PURE__*/React.createElement("div", {
     className: "data-form"
-  }, fieldsData.map((f, idx, arr) => createFieldSet(f, idx, arr.length)));
+  }, validationErrors && validationErrors.length > 0 && showErrors(), fieldsData.map((f, idx, arr) => createFieldSet(f, idx, arr.length)), onSubmit && /*#__PURE__*/React.createElement("button", {
+    onClick: formSubmitted,
+    className: "data-form-submit"
+  }, submitText));
 };
 
 export const useDataForm = formData => {
--- src/react-data-forms.js
+++ src/react-data-forms.js
@@ -3,7 +3,13 @@ import Input from "./Elements/Input";
 import RadioGroup from "./Elements/RadioGroup";
 import Dropdown from "./Elements/Dropdown";
 import DateType from "./Elements/DateType";
-const DataForm = ({fieldsData, onChange, form}) => {
+const DataForm = ({fieldsData, onChange, form, onSubmit=null, submitText=""}) => {
+    
+    //This makes a single array of all of the items in the form.
+    const justFields = fieldsData.flatMap(fs=>fs.fields.map(f=>f));
+
+    const [validationErrors, setValidationErrors] = useState([]);
+
     const [onFieldset, setOnFieldset] = useState(0);
     const elementTypes = {
         Input:Input,
@@ -12,17 +18,46 @@ const DataForm = ({fieldsData, onChange, form}) => {
         DateType:DateType
     }
 
+    //handle validation here
+    const formSubmitted = async () =>{ 
+        //validate first!
+
+        //these are the ones that need to be validated
+        let validateList = justFields.filter(f=>f.validate);
+
+        //for now just dealing with notNull
+        let errors = validateList.map(f=>(f.validate === 'not-null' && !form[f.name]) ? {name:f.name, error:'Empty Value Not Allowed'} :null).filter(f=>f)
+        //TODO Additional validators.
+
+
+        //IF there are errors this will not allow it to be sent
+        if(errors.length > 0) return setValidationErrors(errors);
+
+        //do the submission. If an error is returned add the error here.
+        errors = await onSubmit(form);
+        if(!errors) return;
+        if(errors.length > 0) return setValidationErrors(errors);
+    };
+
+    //TODO 
     const createFieldSet = (data,idx, length)=>(
         <fieldset key={idx} className={idx === onFieldset ? 'open':'closed'}>
             <legend>
-                {data.title}{onFieldset !== idx && <button onClick={()=>setOnFieldset(idx)}>Show Section</button>}
+                {onFieldset !== idx && <button onClick={()=>setOnFieldset(idx)}>{data.title}</button>}
+                {onFieldset === idx && data.title}
             </legend>
             <details open = {idx === onFieldset ? true:false }>
                 <summary style={{display:'none'}}></summary>
                 <div className='form-element-group'>        
                     {data.fields.map(d=>{
                         const E = elementTypes[d.type] || elementTypes.Input;
-                        return (<E key={d.name} {...d} onChange={onChange} value={form[d.name]} />)
+                        const error = validationErrors.find(e=>e.name === d.name )
+                        return (
+                        <div key={d.name} >                            
+                            <E {...d} onChange={onChange} value={form[d.name]} />
+                            {error && <div className="data-form-error"> ^ {d.title} : {error.error}</div>}
+                        </div>
+                        )
                     })}
                 </div> 
                 <div className='form-navigation'>
@@ -33,10 +68,23 @@ const DataForm = ({fieldsData, onChange, form}) => {
         </fieldset>
     );
 
+    //if the error name is found, the error message will show up near that form item.
+    //otherwise it will be a general for error.
+    const showErrors = () => (
+        <div>
+            <div className='data-form-error'>There are Errors.</div>
+        {
+            validationErrors
+            .filter(e=>!justFields.find(f=>e.name===f.name))
+            .map((e,idx)=><div key={idx} className='data-form-error'>{e.name}: {e.error}</div>)
+        }
+    </div>)
 
     return (
-    <div className='data-form'>
+    <div className='data-form' >
+        {(validationErrors && validationErrors.length > 0) && showErrors()}
         {fieldsData.map((f,idx, arr)=>createFieldSet(f,idx, arr.length))}
+        {onSubmit && <button onClick={formSubmitted} className='data-form-submit'>{submitText}</button>}
     </div>)
 };
 
